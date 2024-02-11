ddb0845fa017926e490499f6dbf43a7084667847
Roland-Brice <rows@192.168.1.10>
2024-02-11 18:24:58 +0000

    Création de plusieurs vues différentes

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Heloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+
+def detail(request, question_id):
+    return HttpResponse("Vous cherchez la question %s." % question_id)
+
+def results(request, question_id):
+    response = "Vous cherchez les résultats à la question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "Vote pour la question %s."
+    return HttpResponse(response % question_id)
